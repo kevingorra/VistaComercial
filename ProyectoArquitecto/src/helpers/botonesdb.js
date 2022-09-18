30e041d4f6eb1975bb01e0ad5fd0a4a36f9c9dd7
kevin <kevinvalencia3071029@gmail.com>
2022-09-18 16:09:16 -0500

    se organiza el proyecto V1

---
+++ ProyectoArquitecto/src/helpers/botonesdb.js
@@ -0,0 +1,14 @@
+export let botones = [
+    {nombre:"Categoria",
+     categoria:0
+    },
+    {nombre:"Arquitectura",
+     categoria:1
+    },
+    {nombre:"Alimentos",
+    categoria:2
+    },
+    {nombre:"Productos",
+    categoria:3
+   }
+]
